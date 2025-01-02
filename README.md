b0aad69d4e5e9ffa8e846f1af7cf5c0e59d6c05c
Keanu Joshua Glied <jeralulz@web.de>
2025-01-02 22:29:55 +0100

    Wow

--- README.md
+++ README.md
@@ -3,5 +3,18 @@
 I actually have no exact use case for it but I didn't find anyone having done it either, so here I am.
 This is the thinnest wrapper to make working with Discord Webhooks just a tiny bit less annoying than straight up calling `Invoke-WebRequest -Method Post -ContentType 'application/json' -Body...`
 
+## Usage
+``powershell
+$ ./package.ps1                     # generate tumult.psm1 by running package.ps1
+$ Import-Module ./tumult.psm1       # import the generated Module file and use it
+``
 
-Where am I heading with it? I don't know either.
+Currently there are now exported functions to send a message of anykind. Should you want to use it, fallback to using the `[DiscordWebhook].Send()` function after setting up content and the like.
+
+## Example
+A complete example how to use the webhook is under `./example/` for brevities sake here is a minimal working example
+```powershell
+$ $wh = New-TumultWebhook YOUR_WEBHOOK
+$ $wh.content = 'Text message. Up to 2000 characters.'                                                                                             
+$ $wh.Send() 
+```
--- example/full_example.ps1
+++ example/full_example.ps1
@@ -1,18 +1,18 @@
-$wh = [DiscordWebhook]::new(<YOUR_WEBHOOK_HERE>)
+$wh = New-TumultWebhook <YOUR_WEBHOOK>
 $wh.content = 'Text message. Up to 2000 characters.'
 $wh.username = 'Webhook'
 $wh.avatar_url = 'https://i.imgur.com/4M34hi2.png'
-$em1 = [DiscordEmbed]::new()
-$em1.author = [DiscordAuthor]::new('foo', 'https://www.reddit.com/r/cats/', 'https://i.imgur.com/R66g1Pe.jpg')
+$em1 = New-TumultEmbed
+$em1.author = New-TumultAuthor -name 'foo' -url 'https://www.reddit.com/r/cats/' -icon_url 'https://i.imgur.com/R66g1Pe.jpg'
 $em1.title = 'Title'
 $em1.url = 'https://youtube.com'
 $em1.description = 'Text message. You can use Markdown here. *Italic* **bold** __underline__ ~~strikeout~~ [hyperlink](https://google.com) `code`'
 $em1.color = 15258703
-$em1.fields.Add([DiscordField]::new('Text', 'More text', $true))
-$em1.fields.Add([DiscordField]::new('Even more text', 'yup', $true))
-$em1.fields.Add([DiscordField]::new('Use `"inline": true` parameter, if you want to display fields in the same line.', 'okay...'))
+$em1.fields.Add((New-TumultField -name 'Text' -value 'More text' -inline $true))
+$em1.fields.Add((New-TumultField -name 'Even more text' -value 'yup' -inline $true))
+$em1.fields.Add((New-TumultField -name 'Use `"inline": true` parameter, if you want to display fields in the same line.' -value 'okay...'))
 $em1.thumbnail.url = 'https://i.imgur.com/Grwmc5K.jpeg'
 $em1.image.url = 'https://i.imgur.com/tNXZJUf.jpeg'
-$em1.footer = [DiscordFooter]::new('Woah! So cool! :smirk:', 'https://i.imgur.com/fKL31aD.jpg')
+$em1.footer = New-TumultFooter -text 'Woah! So cool! :smirk:' -icon_url 'https://i.imgur.com/fKL31aD.jpg'
 $wh.embeds.Add($em1)
 $wh.Send()
